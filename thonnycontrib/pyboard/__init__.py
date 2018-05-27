188c2bccd3c84ce9a8a82a5abe42fbbdf08ee9a7
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-05-27 12:04:18 +0300

    misc tweaks

--- thonnycontrib/pyboard/__init__.py
+++ thonnycontrib/pyboard/__init__.py
@@ -1,4 +1,5 @@
-from thonnycontrib.micropython import MicroPythonProxy, MicroPythonConfigPage
+from thonnycontrib.micropython import MicroPythonProxy, MicroPythonConfigPage,\
+    add_micropython_backend
 from thonny import get_workbench, get_runner
 import os
 from thonny import THONNY_USER_BASE
@@ -6,7 +7,7 @@ import subprocess
 from thonny.ui_utils import SubprocessDialog
 import webbrowser
 
-class PyBoardProxy(MicroPythonProxy):
+class PyboardProxy(MicroPythonProxy):
     @property
     def known_usb_vids_pids(self):
         return super().known_usb_vids_pids | {
@@ -16,15 +17,22 @@ class PyBoardProxy(MicroPythonProxy):
             (0xF055, 0x9802), # Pyboard in CDC+MSC mode
         }
 
+    def _supports_directories(self):
+        return True
+    
     def _get_fs_mount_name(self):
         return "PYBFLASH"
         
-    def select_and_upload_micropython(self):
-        webbrowser.open("https://github.com/micropython/micropython/wiki/Pyboard-Firmware-Update")
-
-class PyBoardConfigPage(MicroPythonConfigPage):
+class PyboardConfigPage(MicroPythonConfigPage):
     pass
 
+def select_and_upload_micropython():
+    webbrowser.open("https://github.com/micropython/micropython/wiki/Pyboard-Firmware-Update")
+
+
+
 def load_plugin():
-    get_workbench().set_default("PyBoard.port", "auto")
-    get_workbench().add_backend("PyBoard", PyBoardProxy, "MicroPython on PyBoard", PyBoardConfigPage)
+    add_micropython_backend("PyBoard", PyboardProxy, "MicroPython on pyboard", PyboardConfigPage)
+    get_workbench().add_command("uploadmicropythonpyboard", "tools", "Instructions for uploading MicroPython to pyboard ...",
+                                select_and_upload_micropython,
+                                group=120)
